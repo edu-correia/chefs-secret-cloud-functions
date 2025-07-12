ad4dd1b6bb97c1f7e94ccaa1e2bfbf5fc4ee174b
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2025-07-12 00:00:04 -0300

    :heavy_plus_sign: Add FFMPEG implementation

---
+++ functions/src/services/ffmpeg/FfmpegExternalFileDownloader.ts
@@ -0,0 +1,31 @@
+import ffmpeg from "fluent-ffmpeg";
+
+import { ExternalFileDownloader } from "../../domain/interfaces/ExternalFileDownloader";
+
+const MP3_CODIFICATION = "libmp3lame";
+const AUDIO_QUALITY = 2;
+
+export class FfmpegExternalFileDownloader implements ExternalFileDownloader {
+    async downloadAudioFromOnlineVideo(rawVideoUrl: string, destinationPath: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            if (!rawVideoUrl.startsWith('http')) {
+              return reject(new Error('URL de vídeo inválida.')); // InvalidVideoUrlError
+            }
+        
+            ffmpeg(rawVideoUrl)
+              .noVideo()
+              .audioCodec(MP3_CODIFICATION)
+              .audioQuality(AUDIO_QUALITY)
+              .on('end', () => {
+                // TODO: Add success log
+                resolve();
+              })
+              .on('error', (err) => {
+                // TODO: Add error log
+                // throw new DownloadAudioError
+                reject(err);
+              })
+              .save(destinationPath);
+          });
+    }
+}
