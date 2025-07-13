5f5ab4c10ec946f03263f19e9a9bfa224c2220d1
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2025-07-13 19:17:36 -0300

    :ambulance: Fix tmp folder used in cloud functions

--- functions/src/services/path/PathLocalFileManager.ts
+++ functions/src/services/path/PathLocalFileManager.ts
@@ -1,24 +1,29 @@
 import path from "path";
-import fs from 'fs';
+import fs from "fs";
+import * as os from "os";
 
-import { LocalFileManager } from "../../domain/interfaces/LocalFileManager";
+import {LocalFileManager} from "../../domain/interfaces/LocalFileManager";
 
 export class PathLocalFileManager implements LocalFileManager {
-    generateFilePathOnTemporaryFolder(filePath: string): string {
-        return path.join(__dirname, "..", "..", "..", "tmp", filePath);
-    }
+  generateFilePathOnTemporaryFolder(filePath: string): string {
+    this.ensureTmpDirExists();
+    return path.join(os.tmpdir(), filePath);
+  }
 
-    async deleteFile(filePath: string): Promise<void> {
-        return new Promise((resolve, reject) => {
-            fs.unlink(filePath, (err) => {
-                if (err) {
-                    reject(new Error(`Failed to delete file: ${filePath}`));
-                } else {
-                    resolve();
-                }
-            });
+  async deleteFile(filePath: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          reject(new Error(`Failed to delete file: ${filePath}`));
+        } else {
+          resolve();
         }
-        );
+      });
     }
+    );
+  }
 
+  private ensureTmpDirExists(): void {
+    fs.mkdirSync(os.tmpdir(), { recursive: true });
+  }
 }
